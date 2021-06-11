b4d60d8b8851fb8382aa39cedf5085f714fc0ca3
Adarsh Patel <swe.adarsh@gmail.com>
2021-06-11 00:28:14 -0400

    Style profile section and navbar

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html', title="MLH Fellow - Team 3", url=os.getenv("URL"))
+   return render_template('index.html', title="John Smith", url=os.getenv("URL"))
 
 @app.route('/about')
 def about():
-    return render_template('aboutPage.html', title="MLH Fellow - Team 3", url=os.getenv("URL"))
+    return render_template('aboutPage.html', title="John Smith", url=os.getenv("URL"))
 
 @app.route('/blog')
 def blog():
-    return render_template('blogPage.html', title="MLH Fellow - Team 3", url=os.getenv("URL"))
+    return render_template('blogPage.html', title="John Smith", url=os.getenv("URL"))
 
 @app.route('/projects')
 def projects():
-    return render_template('projectsPage.html', title="MLH Fellow - Team 3", url=os.getenv("URL"))
+    return render_template('projectsPage.html', title="John Smith", url=os.getenv("URL"))
